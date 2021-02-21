eb96baf4f8aafc343981a1a34b054b3094f149a7
Lucas Meyble <lucasmeyble123@gmail.com>
2021-02-21 18:02:57 -0300

    adcionando o conceito de asstes, que no caso é so uma pasta aonde estao os arquivos ativos do programa

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -8,27 +8,9 @@ class Main extends BaseController
 {
 	public function index()
 	{
-		$table = new \CodeIgniter\View\Table();
-
-		$template = [
-			'table_open'         => '<table class="table table-striped table-dark">',
-			'thead_open'         => '<thead class="">',
-			'thead_close'        => '</thead>',
-];
-
-$table->setTemplate($template);
-
-		$data = [
-			['Name', 'Color', 'Size'],
-			['Fred', 'Blue',  'Small'],
-			['Mary', 'Red',   'Large'],
-			['John', 'Green', 'Medium'],
-			['luquinhas', 'felipp', 'rafa']
-		];
-
-
+		
 		echo view('templates/header');
-		echo view('table', ['table' => $table->generate($data)]);
+		echo view('assets');
 		echo view('templates/footer');
 
 	}
---
+++ app/Views/assets.php
@@ -0,0 +1,2 @@
+<h1>assets</h1>
+<img src="<?php echo base_url('assets/images/world-360_1080_cover.jpg')?>">
