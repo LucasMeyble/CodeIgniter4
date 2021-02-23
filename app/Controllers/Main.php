ee1860762c5ac60188357b5c59a4242483c3dc83
Lucas Meyble <lucasmeyble123@gmail.com>
2021-02-22 21:39:14 -0300

    conceito de session

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -6,33 +6,26 @@ use CodeIgniter\Controller;
 
 class Main extends BaseController
 {
-	public function index()
-	{
-		
-		echo view('templates/header');
-		echo view('home');
-		echo view('templates/footer');
+	public function index(){
+
+		// os dados do session sao guardados em cache, por isso mesmo q o email esteja comentado ele aparesce no print_r
+
+		session()->set('email', 'test@example.com');
+		session()->set('nome', 'Lucas');
+
+		echo '<pre>';
+		print_r(session()->get('nome'));
+		echo '</br>';
+		print_r(session()->get('email'));
+
+		return redirect()->to('main/teste');
 
 	}
 
-	public function servicos()
-	{
-
-		$a = true;
-
-		if($a){
-			return redirect()->to('redirecionamento');
-		}
-
-		echo view('templates/header');
-		echo view('servicos');
-		echo view('templates/footer');
+	public function teste(){
+		echo 'o nome do usuario é: ' . session()->get('nome');
+		echo '</br>';
+		echo 'o email do usuario é: ' . session()->get('email'); 
 	}
-	
-	public function redirecionamento(){
-		echo 'vc foi redirecionado';
-	}
-
-
 }
 ?>
