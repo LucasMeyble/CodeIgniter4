9bedef571b98fa517906af5d62134d4caf5ed37b
Lucas Meyble <lucasmeyble123@gmail.com>
2021-02-24 16:03:01 -0300

    sistema de login e logOut, ultilizando session com oforma de cache

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -7,30 +7,37 @@ use CodeIgniter\Controller;
 class Main extends BaseController
 {
 	public function index(){
-		// os dados do session sao guardados em cache, por isso mesmo q o email esteja comentado ele aparesce no print_r
-
-		session()->set('usuario', 'Lucas');
-		session()->set('nome', 'Pedro');
-		session()->set('email', 'pedro@gmail.com');
-		echo session()->get('usuario');
-		echo '</br>';
-		
-
-		//forma de fazer um if perguntando se existe uma session com uma determinada chave, no caso o has
-		if(session()->has('email')){
-			echo 'sim';
-		}else{
-			echo 'nao';
-		}
-
-
+		echo 'index';
 		$this->printSession();
 	}
 
-	public function home(){
-	
+	//-----------------------------------------------------------------------------
+	public function login(){
+		session()->set([
+			'nome' => 'Lucas',
+			'email' => 'lucas@gmail.com'
+		]);
+
+		echo 'login';
+		$this->printSession();
 	}
 
+	//-----------------------------------------------------------------------------
+	public function usuario(){
+		if(session()->has('nome')){
+			echo 'Existe usuario: '. session()->get('nome');
+		}else{
+			echo 'Nao existe o usuario';
+		}
+	}
+
+	//-----------------------------------------------------------------------------
+	public function logOut(){
+		session()->destroy();
+		return redirect()->to(site_url('main'));
+	}
+
+	//-----------------------------------------------------------------------------
 	//função para da um print_r com os dados da session da função q a chamar
 	private function printSession(){
 		echo '<pre>';
