1049e94df275f012fdabc2c1f05b2b5a78bef0e4
Lucas Meyble <lucasmeyble123@gmail.com>
2021-02-24 15:16:54 -0300

    session e principais metodos

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -7,25 +7,35 @@ use CodeIgniter\Controller;
 class Main extends BaseController
 {
 	public function index(){
-
 		// os dados do session sao guardados em cache, por isso mesmo q o email esteja comentado ele aparesce no print_r
 
-		session()->set('email', 'test@example.com');
-		session()->set('nome', 'Lucas');
+		session()->set('usuario', 'Lucas');
+		session()->set('nome', 'Pedro');
+		session()->set('email', 'pedro@gmail.com');
+		echo session()->get('usuario');
+		echo '</br>';
+		
 
+		//forma de fazer um if perguntando se existe uma session com uma determinada chave, no caso o has
+		if(session()->has('email')){
+			echo 'sim';
+		}else{
+			echo 'nao';
+		}
+
+
+		$this->printSession();
+	}
+
+	public function home(){
+	
+	}
+
+	//função para da um print_r com os dados da session da função q a chamar
+	private function printSession(){
 		echo '<pre>';
-		print_r(session()->get('nome'));
-		echo '</br>';
-		print_r(session()->get('email'));
-
-		return redirect()->to('main/teste');
-
+		print_r(session()->get());
 	}
 
-	public function teste(){
-		echo 'o nome do usuario é: ' . session()->get('nome');
-		echo '</br>';
-		echo 'o email do usuario é: ' . session()->get('email'); 
-	}
 }
 ?>
