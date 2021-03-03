9c26710b5e53bf81dca8ae2de5b662912a3a034c
Lucas Meyble <lucasmeyble123@gmail.com>
2021-03-02 22:51:49 -0300

    configuração de dataBase com select e mostrando o resultado em tela

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -7,42 +7,13 @@ use CodeIgniter\Controller;
 class Main extends BaseController
 {
 	public function index(){
-		echo 'index';
-		$this->printSession();
-	}
+		$db = \Config\Database::connect();
+		                                                  //getResultArray
+		$results = $db->query("SELECT * FROM cliente_tbl")->getResultObject();
+		$db->close();
 
-	//-----------------------------------------------------------------------------
-	public function login(){
-		session()->set([
-			'nome' => 'Lucas',
-			'email' => 'lucas@gmail.com'
-		]);
-
-		echo 'login';
-		$this->printSession();
-	}
-
-	//-----------------------------------------------------------------------------
-	public function usuario(){
-		if(session()->has('nome')){
-			echo 'Existe usuario: '. session()->get('nome');
-		}else{
-			echo 'Nao existe o usuario';
-		}
-	}
-
-	//-----------------------------------------------------------------------------
-	public function logOut(){
-		session()->destroy();
-		return redirect()->to(site_url('main'));
-	}
-
-	//-----------------------------------------------------------------------------
-	//função para da um print_r com os dados da session da função q a chamar
-	private function printSession(){
 		echo '<pre>';
-		print_r(session()->get());
-	}
-
+		var_dump($results);
+}
 }
 ?>
