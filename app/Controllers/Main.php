a73cecc1966d1c701e50b41c99398368295e8755
Lucas Meyble <lucasmeyble123@gmail.com>
2021-03-02 23:00:39 -0300

    o mesmo resultado de formas diferentes

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -9,11 +9,20 @@ class Main extends BaseController
 	public function index(){
 		$db = \Config\Database::connect();
 		                                                  //getResultArray
-		$results = $db->query("SELECT * FROM cliente_tbl")->getResultObject();
+		$results = $db->query("SELECT * FROM cliente_tbl");
 		$db->close();
 
-		echo '<pre>';
-		var_dump($results);
+		//oq quero mostrar com isso é que podemos obter o mesmo resultado de vavrias formas, vai de cada um escoher 
+		//o metodo a ser ultilizado
+		foreach ($results->getResult() as $row){
+			echo '<p>'. $row->nome . ' - '. $row->email .'</p>';
+		}
+
+		foreach ($results->getResult('array') as $row){
+			echo '<p>'. $row['nome'] . ' - '. $row['email'] .'</p>';
+		}
+
+	
 }
 }
 ?>
