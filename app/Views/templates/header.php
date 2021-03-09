b03337d00dc8c06a954b98fba5d96319113e605f
Lucas Meyble <lucasmeyble123@gmail.com>
2021-03-09 09:49:00 -0300

    refazendo rotas de assets para o pc do trabalho

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link href=<?php echo base_url('assets/css/bootstrap.min.css')?> rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <link href=<?php echo base_url('assets/css/bootstrap.min.css')?> rel="stylesheet" integrity="" crossorigin="anonymous">
 </head>
 <body>
   
